0534f7b41a22c25413f12f12e44ab13ca5821255
platon-afk10d <platon.sinitsin@gmail.com>
2019-12-02 16:31:56 +0300

    comlete task 2

--- src/main/java/lesson01/part1/Task02.java
+++ src/main/java/lesson01/part1/Task02.java
@@ -19,7 +19,10 @@ public class Task02 {
      * 5. Попробуй удалить метод main, в пустом месте написать psvm и нажать tab.
      */
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!\nHello World!\nHello World!");
+        public static void psvm(String[] args) {
+        String hi = "Hello school!";
+        System.out.println(hi);
+        System.out.println(hi);
+        System.out.println(hi);
     }
 }

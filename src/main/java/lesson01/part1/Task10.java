c554fc68c9019a71468d00d656f2b020a597358a
platon-afk10d <platon.sinitsin@gmail.com>
2019-12-08 12:44:08 +0300

    complete task№10

--- src/main/java/lesson01/part1/Task10.java
+++ src/main/java/lesson01/part1/Task10.java
@@ -15,5 +15,17 @@ public class Task10 {
      */
 
     public static void main(String[] args) {
+        function checkTime (beg, end)
+        {
+            var s = 60,
+                    d = ':',
+                    b = beg.split (d), b = b [0]* s * s + b [1] * s + +b [2],
+                    e = end.split (d), e = e [0]* s * s + e [1] * s + +e [2],
+                    t = new Date, t = t.getHours () * s * s + t.getMinutes () * s + t.getSeconds ();
+            return (t >= b && t <= e);
+        }
+
+        var rez = checkTime ('15:00:00', '15:30:00');
+        alert (rez);
     }
 }
